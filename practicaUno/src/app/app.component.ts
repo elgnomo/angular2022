94fa563a4f9306ff3725895aabfdb9e3ca8db53b
elgnomo <efrainantonio@ideashappy.com>
2022-03-29 04:27:34 -0600

    Event Binding Listener

--- practicaUno/src/app/app.component.ts
+++ practicaUno/src/app/app.component.ts
@@ -24,4 +24,15 @@ export class AppComponent {
   addButton() {
     this.person.age += 1;
   }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
