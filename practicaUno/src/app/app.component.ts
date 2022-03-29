f1f14aa5480fbf24e778f089ac30de3678c37f42
elgnomo <efrainantonio@ideashappy.com>
2022-03-29 03:45:23 -0600

    String interpolation

--- practicaUno/src/app/app.component.ts
+++ practicaUno/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',// ya esta conectado
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'practicaUno';
+  name = 'Gnomo';
+  age = 36;
+  img = 'https://source.unsplash.com/random'
 }
---
+++ practicaUno/src/app/recap.ts
@@ -0,0 +1,10 @@
+const username: string = 'gnomo';
+// const username: string | number = 'gnomo';
+const age: number = 36;
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+sum(1, 2);
+
